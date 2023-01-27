0bcf1225efded9023c9e444316002cfd69e36734
longf <longf@DESKTOP-2GJ1G8J>
2023-01-27 22:12:57 +0100

    Add project files.

---
+++ Inlämningsuppgift 5.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace uppgift_5_6
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            long g = 1;
+            Console.WriteLine("Skriv in talen du vill multiplicera separerade med asterisker '*' ");
+           string Tal =Console.ReadLine();
+            string[] separation = Tal.Split('*');
+
+           
+            for (long i = 0; i < separation.Length; i++)
+            {
+                long[] b =new long[separation.Length];
+                b [i] = long.Parse(separation[i]);
+                Console.WriteLine(b[i]);
+                g = g * b[i];
+            }
+            Console.Write("Summan av talen blir " + g);
+
+            
+            
+        }
+    }
+}
+
